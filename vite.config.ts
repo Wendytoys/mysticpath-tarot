c88d3690aecab3092fe68495825770646460a264
wendytoys <vasuherog@gmail.com>
2025-07-28 22:08:47 +0530

    Fix: Correct vite config for env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY),
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
